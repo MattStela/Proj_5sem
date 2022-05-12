e8f38dba6f8969c8fe67e5ad51c98a365f5b075f
Zanad <daniel.zanad.dz@gmail.com>
2022-05-12 17:09:11 -0300

    Adiciona showProfile em colaboradores.js

--- Scripts/colaboradores.js
+++ Scripts/colaboradores.js
@@ -1,14 +1,17 @@
 let experience = document.getElementById('professional_experience');
 let about = document.getElementById('about_text');
+let profileImage = document.getElementById('profile_image_name');
+let profileAgeRole = document.getElementById('profile_age_role');
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
-async function getData() {
+async function getDataAndShowProfile() {
   fetch("../Scripts/data/dadosColaboradores.json").then(res => {
     return res.json()
   }).then(jsonData => {
     showAbout(jsonData);
     showExperience(jsonData);
+    showProfile(jsonData)
     return 0
   })
 }
@@ -28,4 +31,17 @@ async function showExperience(data) {
   });
 }
 
-getData()
+async function showProfile(data) {
+  profileImage.innerHTML = `
+  <img width="45%" src="${data[id].profileImage}" alt="${data[id].name}"><br>
+  <p>${data[id].name}</p>
+  `;
+
+  profileAgeRole.innerHTML = `
+  <p> ${data[id].name.split(' ')[0]} ,${data[id].about.age} </p>
+  <p> ${data[id].about.role}  </p>
+`
+}
+
+
+getDataAndShowProfile();
